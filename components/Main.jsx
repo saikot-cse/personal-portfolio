a6d943c66d2aee975ce9d90c5b81319d44232faa
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-06-09 07:23:43 +0600

    Project section temporary added

--- components/Main.jsx
+++ components/Main.jsx
@@ -37,6 +37,10 @@ export const Main = () => {
           <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
           I&apos;m a MERN Stack Developer in Bangladesh. My specialties include designing Interactive websites with CRUD application. I love what I do and truly take pride in producing high-quality work as well as great customer service to my clients.
           </p>
+          <a
+              href='https://doc-0k-00-docs.googleusercontent.com/docs/securesc/r1iluhb0ij02iie0004ufnfpistm7r68/sqbigfv3t5k6i3sl11dup1pker1tb2gc/1654736025000/17869412803603710737/17869412803603710737/1KkfeLfSQGejRE_ZPg3Jzz44iLDt0PR3E?e=download&ax=ACxEAsbA9oXMSde1Acfqyn7YDbKkAlezECVFMovGCjZakcrf6rM-f0CHV4h225n4091e4T1BuVpqtKa8BVgVVlzVWM9bfVCjH1j17i7to2D3xsQVHWJmp4eIk53srKEkGpOcmqPJ37pg16sC5iS2p9B-KhAg2aTLLJZhAcVVpIPVpLO3FjBAXioI5fhNdrjZmsmuHr6YYJKn8XlZlcOzBCm_Fv35iz1UpDh_Gv-Og1_4f0qnfP-INx5dbrI6AzSiHdAswcrKUCRClPQsBOcEdvoylX9G6zRMYKoHOKCvGTsyNsDWUF42Qhd7lIJ0azp6hlHzyfFFngiscKV5XPdtnS669HCsHz-XqCwhqw8H6J2bF-tbp-h_46zUa-dcwMtzErO_sbdl0ljj2yW6R7kZpwidcq6PFGptaHnXcik-Ppjhv_rJ5X84fU_zVUto8VQCteZOINXYJhA7gww0D-AD8_5CPcs4r0NDotNguovsU7nOs2ToFrJnjx103gXzylAGv1Bafv36RV0vYSCInkb-E0JuUK5ht2RBUq5WBD91sBQQDmNgoX_Cu0OfzVMCbpxU3xDkQW_k0O0wwx4mgAxtX-b5nWFD91-GujEv8jLO03cDrJlDK2zwBFbZLJre71TRwU1J8f34owAmn7RzB0Lm8itNPnkbrinuU_4Kzlp88nZiJ7dze6FMswWdQPCreqtVRnoOszwuJnbY9qCfjrJIU788At-EA7aOX-jQYpOTT-pS9QAs69xKJlweCWq018aftzHOvFtkYK87yaaSkD-FlJIbtjw&authuser=2&nonce=5mpfhjtkm8hqq&user=17869412803603710737&hash=4cojumd1m8njphb5uu3e3vkmvpjf37la'
+              target='_blank' rel='noopener noreferrer'
+            ><button className="block mx-auto px-4 py-2 rounded-full">Download Resume</button></a>
           <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
             <Link
               href='https://www.linkedin.com/in/saifullah-mansur/'
---
+++ components/Projects.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import estroGadget from '../public/assets/projects/estro-gadget.png';
+import proTechGear from '../public/assets/projects/pro-tech-gear.png'
+import diligentDeveloper from '../public/assets/projects/diligate-developer.png'
+import { ProjectItem } from './Projectitem';
+
+
+export const Projects = () => {
+  return (
+    <div id='projects' className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What I&apos;ve Built</h2>
+        <div className='grid md:grid-cols-3 gap-8'>
+          <ProjectItem
+            title='Estro Gadget'
+            backgroundImg={estroGadget}
+            projectUrl='/estroGadget'
+          />
+          <ProjectItem
+            title='Pro Tech Gear'
+            backgroundImg={proTechGear}
+            projectUrl='/proTechGear'
+          />
+          <ProjectItem
+            title='Diligent Developer'
+            backgroundImg={diligentDeveloper}
+            projectUrl='/diligentDeveloper'
+          />
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
